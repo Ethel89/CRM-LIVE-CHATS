52b27beccb965cd3422bf8115155c355a9b47b59
VA Quad <ethelinyama.va@gmail.com>
2024-09-14 01:20:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript is working!');
+});
+git clone https://github.com/Ethel89/crm-integration-site.git
+cd crm-integration-site
